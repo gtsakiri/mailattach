85fbcbe0756e71d75264811c7af8053bed9a012f
gtsakiri <tsakiridis@serres.gr>
2021-03-26 12:26:29 +0200

    Commit OK!

--- mailacc/views.py
+++ mailacc/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
 
 from mailattach import settings
 from .models import Mailbox
-from django.core.mail import send_mail
+from django.core.mail import EmailMessage
 
 # Create your views here.
 def mailbox_list(request):
@@ -14,14 +15,24 @@ def mailbox_list(request):
     return render(request, 'mailacc/mailbox_list.html', context)
 
 def sendMessage(request):
-    paramsubject = request.GET.get('msgtitle')
-    paramtext = request.GET.get('msgtext')
-    paramreceivers = request.GET.get('multiReceivers')
+    if request.method=='POST':
+        paramsubject = request.POST.get('msgtitle')
+        paramtext = request.POST.get('msgtext')
+        paramreceivers = request.POST.get('multiReceivers')
+        attachlist = request.FILES.get('customfile')
 
-    subject = paramsubject
-    message = paramtext
-    email_from = settings.EMAIL_HOST_USER
-    recipient_list = paramreceivers.split(",")
-    send_mail(subject, message, email_from, recipient_list)
 
-    return render(request, 'mailacc/Sendresult.html')
+        subject = paramsubject
+        message = paramtext
+        email_from = settings.EMAIL_HOST_USER
+        recipient_list = paramreceivers.split(",")
+        if  paramreceivers:
+            mail=EmailMessage(subject, message, email_from, recipient_list)
+            mail.attach(attachlist.name, attachlist.read(), attachlist.content_type)
+            mail.send()
+            context={'msg':attachlist}
+        else:
+            context = {'msg':'No recipients!',}
+
+
+    return render(request, 'mailacc/Sendresult.html',context)
